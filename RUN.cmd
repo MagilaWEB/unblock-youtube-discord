d0cb7b413a27fd673ad91c08c34724ee3eb8d4b1
MagilaWEB <78775512+MagilaWEB@users.noreply.github.com>
2024-10-11 23:52:20 +0300

    -Отключить дебаг.

--- RUN.cmd
+++ RUN.cmd
@@ -1,6 +1,6 @@
 ECHO off
 chcp 1251
-set ARGS=--debug=1 --wf-tcp=80,443 --wf-udp=443,50000-65535 ^
+set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-65535 ^
 --filter-udp=443 --hostlist="%~dp0russia-blacklist.txt" --dpi-desync=fake --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%~dp0bin\quic_initial_www_google_com.bin" --new ^
 --filter-udp=50000-65535 --dpi-desync=fake,tamper --dpi-desync-any-protocol desync-fooling=md5sig --dpi-desync-fake-quic="%~dp0bin\quic_initial_www_google_com.bin" --new ^
 --filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
