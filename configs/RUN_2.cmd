57d5d3f00efc919819d3184ebbdaa9a52f4fea4f
MagilaWEB <78775512+MagilaWEB@users.noreply.github.com>
2025-05-27 09:51:13 +0300

    -Обновлён профиль 2 с дополнительным фильтром по ip.

--- configs/RUN_2.cmd
+++ configs/RUN_2.cmd
@@ -2,10 +2,13 @@ ECHO off
 chcp 1251
 @REM 2 вариант, если не работает базовый.
 set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-65535 ^
---filter-udp=443 --hostlist="%~dp0russia-blacklist.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%~dp0..\bin\quic_initial_www_google_com.bin" --new ^
---filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol=1 --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
---filter-tcp=80 --dpi-desync=fake,split2 --dpi-desync-autottl=1 --dpi-desync-fooling=md5sig --new ^
---filter-tcp=443 --hostlist="%~dp0russia-blacklist.txt" --dpi-desync=split2 --dpi-desync-split-seqovl=652 --dpi-desync-split-pos=2 --dpi-desync-fake-tls="%~dp0..\bin\tls_clienthello_www_google_com.bin"
+--filter-udp=443 --hostlist="%~dp0russia-blacklist.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%~dp0..\bin\quic_initial_www_google_com.bin" --new ^
+--filter-udp=50000-65535 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new ^
+--filter-tcp=80 --hostlist="%~dp0russia-blacklist.txt" --dpi-desync=fake,fakedsplit --dpi-desync-autottl=1 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --hostlist="%~dp0russia-blacklist.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --new ^
+--filter-udp=443 --ipset="%~dp0ip-blacklist.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%~dp0..\bin\quic_initial_www_google_com.bin" --new ^
+--filter-tcp=80 --ipset="%~dp0ip-blacklist.txt" --dpi-desync=fake,fakedsplit --dpi-desync-autottl=1 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --ipset="%~dp0ip-blacklist.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig
 
 set SRVCNAME=unblock1
 
