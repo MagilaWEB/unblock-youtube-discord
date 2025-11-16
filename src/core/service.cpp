56d57f9d0af1437f944a8fbc8afafb55c3d44324
MagilaWEB <78775512+MagilaWEB@users.noreply.github.com>
2025-11-16 20:23:15 +0300

    - Доработка класса сервисов.

--- src/core/service.cpp
+++ src/core/service.cpp
@@ -1,4 +1,5 @@
 #include "service.h"
+#include "timer.h"
 
 Service::~Service()
 {
@@ -24,13 +25,22 @@ std::string Service::getName() const
 	return _name;
 }
 
+const Service::Config& Service::getConfig()
+{
+	CRITICAL_SECTION_RAII(lock);
+
+	update();
+
+	return config;
+}
+
 bool Service::isRun()
 {
 	CRITICAL_SECTION_RAII(lock);
 
 	update();
 
-	return sc_status.dwCurrentState == SERVICE_START_PENDING || sc_status.dwCurrentState == SERVICE_RUNNING;
+	return config.sc_status.dwCurrentState == SERVICE_START_PENDING || config.sc_status.dwCurrentState == SERVICE_RUNNING;
 }
 
 void Service::create()
@@ -87,7 +97,7 @@ void Service::start()
 
 	update();
 
-	if (sc_status.dwCurrentState != SERVICE_STOPPED && sc_status.dwCurrentState != SERVICE_STOP_PENDING)
+	if (config.sc_status.dwCurrentState != SERVICE_STOPPED && config.sc_status.dwCurrentState != SERVICE_STOP_PENDING)
 	{
 		InputConsole::textError("служба [%s] не остановлена или в прогрессе остановки, запустить не возможно!", _name.c_str());
 		return;
@@ -102,21 +112,26 @@ void Service::start()
 	InputConsole::textPlease("подождите окончания запуска службы [%s]", true, false, _name.c_str());
 
 	bool send_start = false;
-	u32	 i_start	= 0;
+
+	Timer timer;
+	timer.start();
+
 	do
 	{
 		send_start = StartService(sc, static_cast<u32>(args.size()), args.data());
-		if (i_start++ > 8)
+
+		// We try for 5 seconds, otherwise we interrupt.
+		if (timer.getElapsed_sec() > 5.f)
 		{
 			InputConsole::textError("истекло время ожидания запуска службы [%s], процесс запуска прерван, причина не известна!", _name.c_str());
 			return;
 		}
 
-		using namespace std::chrono;
 		if (!send_start)
 		{
-			// wait 3 second
-			std::this_thread::sleep_for(3'000ms);
+			// wait
+			using namespace std::chrono;
+			std::this_thread::sleep_for(300ms);
 		}
 	} while (!send_start);
 
@@ -142,7 +157,7 @@ void Service::start()
 		}
 	);
 
-	if (sc_status.dwCurrentState == SERVICE_RUNNING)
+	if (config.sc_status.dwCurrentState == SERVICE_RUNNING)
 		InputConsole::textOk("служба [%s] запущена.", _name.c_str());
 	else
 		InputConsole::textWarning("служба [%s] была запущена но неожиданно сменила свой статус.", _name.c_str());
@@ -153,15 +168,58 @@ void Service::update()
 	if (!sc)
 		return;
 
-	bool q_service =
-		QueryServiceStatusEx(sc, SC_STATUS_PROCESS_INFO, reinterpret_cast<LPBYTE>(&sc_status), sizeof(SERVICE_STATUS_PROCESS), &_dw_bytes_needed);
+	DWORD dw_bytes_needed{ 0 };
+
+	bool q_service = QueryServiceStatusEx(
+		sc,
+		SC_STATUS_PROCESS_INFO,
+		reinterpret_cast<LPBYTE>(&config.sc_status),
+		sizeof(SERVICE_STATUS_PROCESS),
+		&dw_bytes_needed
+	);
 
 	if (!q_service)
 	{
 		CloseServiceHandle(sc);
 		sc				 = nullptr;
-		sc_status		 = {};
-		_dw_bytes_needed = 0;
+		config.sc_status = {};
+		return;
+	}
+
+	dw_bytes_needed = 0;
+
+	LPQUERY_SERVICE_CONFIGA service_config = nullptr;
+	u32						size_buffer{ 0 };
+	while (true)
+	{
+		if (QueryServiceConfig(sc, service_config, size_buffer, &dw_bytes_needed))
+		{
+			if (service_config)
+			{
+				config.type				= static_cast<u32>(service_config->dwServiceType);
+				config.start_type		= static_cast<u32>(service_config->dwStartType);
+				config.tag_id			= static_cast<u32>(service_config->dwTagId);
+				config.start_name		= service_config->lpServiceStartName;
+				config.display_name		= service_config->lpDisplayName;
+				config.load_order_group = service_config->lpLoadOrderGroup;
+				config.binary_path		= service_config->lpBinaryPathName;
+
+				free(service_config);
+			}
+
+			break;
+		}
+
+		u32 err = GetLastError();
+		if (service_config && ERROR_MORE_DATA != err)
+		{
+			free(service_config);
+			break;
+		}
+
+		size_buffer += dw_bytes_needed;
+		free(service_config);
+		service_config = reinterpret_cast<LPQUERY_SERVICE_CONFIGA>(malloc(size_buffer));
 	}
 }
 
@@ -192,14 +250,14 @@ void Service::stop()
 
 	bool stopped = false;
 
-	if (sc_status.dwCurrentState != SERVICE_STOPPED)
+	if (config.sc_status.dwCurrentState != SERVICE_STOPPED)
 	{
 		InputConsole::textPlease("подождите окончания остановки службы [%s]", true, false, _name.c_str());
 
 		auto service_stop = [this, &stopped]
 		{
 			stopped					= true;
-			const bool send_control = ControlService(sc, SERVICE_CONTROL_STOP, reinterpret_cast<LPSERVICE_STATUS>(&sc_status));
+			const bool send_control = ControlService(sc, SERVICE_CONTROL_STOP, reinterpret_cast<LPSERVICE_STATUS>(&config.sc_status));
 			ASSERT_ARGS(send_control, "Failed to send a request to stop the service [%s]!", _name.c_str());
 		};
 
@@ -218,7 +276,7 @@ void Service::stop()
 
 	update();
 
-	if (stopped && sc_status.dwCurrentState == SERVICE_STOPPED)
+	if (stopped && config.sc_status.dwCurrentState == SERVICE_STOPPED)
 		InputConsole::textOk("служба [%s] остановлена.", _name.c_str());
 }
 
@@ -247,23 +305,77 @@ void Service::close()
 
 	CloseServiceHandle(sc);
 	sc				 = nullptr;
-	sc_status		 = {};
-	_dw_bytes_needed = 0;
+	config.sc_status = {};
+}
+
+void Service::allService(std::function<void(std::string)>&& callback)
+{
+	static CriticalSection lock;
+
+	CRITICAL_SECTION_RAII(lock);
+
+	if (!_sc_manager)
+		_sc_manager = OpenSCManager(nullptr, nullptr, SC_MANAGER_ALL_ACCESS);
+
+	if (_sc_manager)
+	{
+		LPENUM_SERVICE_STATUSA service_all	  = nullptr;
+		DWORD				   buffer_size	  = 0;
+		DWORD				   service_needed = 0;
+
+		while (true)
+		{
+			DWORD service_count = 0;
+
+			if (EnumServicesStatus(_sc_manager, SERVICE_WIN32, SERVICE_STATE_ALL, service_all, buffer_size, &service_needed, &service_count, nullptr))
+			{
+				if (service_all)
+				{
+					for (u32 i = 0; i < service_count; i++)
+					{
+						std::string name{ service_all[i].lpServiceName };
+						callback(name.size() > 0 ? name : service_all[i].lpDisplayName);
+					}
+
+					free(service_all);
+				}
+				break;
+			}
+
+			u32 err = GetLastError();
+			if (ERROR_MORE_DATA != err)
+			{
+				free(service_all);
+				break;
+			}
+
+			buffer_size += service_needed;
+			free(service_all);
+			service_all = reinterpret_cast<LPENUM_SERVICE_STATUSA>(malloc(buffer_size));
+		}
+	}
+}
+
+std::list<std::string> Service::allService()
+{
+	std::list<std::string> list_service{};
+	allService([&list_service](std::string service_name) { list_service.push_back(service_name); });
+	return list_service;
 }
 
 void Service ::_waitStatusService(DWORD check_state, DWORD check_stat_end, std::function<void()>&& send_timeout_check)
 {
 	_dw_start_time = GetTickCount64();
 
-	while (sc_status.dwCurrentState == check_state)
+	while (config.sc_status.dwCurrentState == check_state)
 	{
-		_dw_wait_time = std::clamp<DWORD>(sc_status.dwWaitHint / 10, 1'000, 10'000);
+		_dw_wait_time = std::clamp<DWORD>(config.sc_status.dwWaitHint / 10, 1'000, 10'000);
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(_dw_wait_time));
 
 		update();
 
-		if (sc_status.dwCurrentState == check_stat_end)
+		if (config.sc_status.dwCurrentState == check_stat_end)
 			break;
 
 		if ((GetTickCount64() - _dw_start_time) > _dw_timeout)
@@ -281,5 +393,5 @@ void Service::_initScManager()
 	if (!_sc_manager)
 		_sc_manager = OpenSCManager(nullptr, nullptr, SC_MANAGER_ALL_ACCESS);
 
-	ASSERT_ARGS(_sc_manager, "Couldn't open SCManager to interact with system services!", _name.c_str());
+	ASSERT_ARGS(_sc_manager, "Couldn't open SCManager to interact with system services!");
 }
--- src/core/service.h
+++ src/core/service.h
@@ -3,31 +3,46 @@
 
 class CORE_API Service final
 {
-	const static inline ULONGLONG _dw_timeout{ 30'000 };
-	static inline SC_HANDLE		  _sc_manager{ nullptr };
+	constexpr static ULONGLONG _dw_timeout{ 30'000 };
+	static inline SC_HANDLE	   _sc_manager{ nullptr };
 
 	CriticalSection lock;
 
 	std::string				 _name{ "" };
 	std::string				 _description{ "" };
 	std::vector<std::string> _args;
-	DWORD					 _dw_bytes_needed{ 0 };
 	ULONGLONG				 _dw_start_time{ GetTickCount64() };
 	ULONGLONG				 _dw_wait_time{ 0 };
 
+protected:
+	struct Config
+	{
+		u32 type{ 0 };
+		u32 start_type{ 0 };
+		u32 tag_id{ 0 };
+
+		std::string			  binary_path{ "" };
+		std::string			  start_name{ "" };
+		std::string			  display_name{ "" };
+		std::string			  load_order_group{ "" };
+
+		SERVICE_STATUS_PROCESS sc_status{};
+	};
+
+	Config config{};
+
 public:
-	
 	const std::filesystem::path file_name;
 	Service(const pcstr name) : _name(name), file_name(std::filesystem::path("")) {}
 	Service(const pcstr name, pcstr _file_name) : _name(name), file_name(std::filesystem::path(_file_name)) {}
 	~Service();
 
-	SC_HANDLE			   sc{ nullptr };
-	SERVICE_STATUS_PROCESS sc_status{};
+	SC_HANDLE sc{ nullptr };
 
-	void setName(std::string new_name);
-	void setDescription(pcstr description);
-	std::string getName() const;
+	void		  setName(std::string new_name);
+	void		  setDescription(pcstr description);
+	std::string	  getName() const;
+	const Config& getConfig();
 
 	bool isRun();
 
@@ -43,6 +58,9 @@ public:
 	void remove();
 
 	void close();
+
+	static void					  allService(std::function<void(std::string)>&& callback);
+	static std::list<std::string> allService();
 
 private:
 	void _initScManager();
